99f43a9c8bc6c378cc2b95f879210241072211f2
Oliver Chamberlain <ochamberlai2@gmail.com>
2025-05-24 19:32:12 +0100

    Setting goal + start tiles

--- src/app/components/grid.ts
+++ src/app/components/grid.ts
@@ -1,6 +1,5 @@
 import { Container, Graphics } from "pixi.js";
 import { Tile } from "./tile";
-import { rect } from "motion/react-client";
 
 export class Grid extends Container {
   public rows: number;
@@ -8,12 +7,14 @@ export class Grid extends Container {
   public tileWidth: number;
   public tileHeight: number;
   public tiles: Tile[][];
+  public tilesFlattened: Tile[];
 
   constructor(
     rows: number,
     cols: number,
     tileWidth: number,
-    tileHeight: number
+    tileHeight: number,
+    cellClickHandler: (tile: Tile) => void
   ) {
     super();
     this.rows = rows;
@@ -21,11 +22,12 @@ export class Grid extends Container {
     this.tileWidth = tileWidth;
     this.tileHeight = tileHeight;
     this.tiles = [];
+    this.tilesFlattened = [];
 
-    this.createGrid();
+    this.createGrid(cellClickHandler);
 
-    const rectHeight = this.tileHeight * this.rows;
-    const rectWidth = this.tileWidth * this.cols;
+    const rectHeight = this.tileHeight * this.rows - 1;
+    const rectWidth = this.tileWidth * this.cols - 1;
     const graphics = new Graphics()
       .rect(0, 0, rectWidth, rectHeight)
       .stroke("white")
@@ -33,17 +35,24 @@ export class Grid extends Container {
     this.addChild(graphics);
   }
 
-  private createGrid() {
+  private createGrid(cellClickHandler: (tile: Tile) => void) {
     for (let row = 0; row < this.rows; row++) {
       const tileRow: Tile[] = [];
       for (let col = 0; col < this.cols; col++) {
-        const tile = new Tile(col, row, this.tileWidth, this.tileHeight);
+        const tile = new Tile(col, row, this.tileWidth, this.tileHeight, this);
         tile.x = col * this.tileWidth;
         tile.y = row * this.tileHeight;
+        tile.interactive = true;
+        tile.eventMode = "static";
+        tile.on("pointerdown", (event: object) => {
+          console.log("clicked", event);
+          cellClickHandler(tile);
+        });
         tileRow.push(tile);
         this.addChild(tile);
       }
       this.tiles.push(tileRow);
     }
+    this.tilesFlattened = this.tiles.flat();
   }
 }
--- src/app/screens/djikstra/DjikstraScreen.ts
+++ src/app/screens/djikstra/DjikstraScreen.ts
@@ -1,7 +1,14 @@
 import { Container } from "pixi.js";
 import { Button } from "../../ui/Button";
-
+import type { Ticker } from "pixi.js";
 import { Grid } from "../../components/grid";
+import { Tile } from "../../components/tile";
+
+export enum Mode {
+  SET_START,
+  SET_GOAL,
+  NONE,
+}
 
 export class DjikstraScreen extends Container {
   public static assetBundles = ["main"];
@@ -9,12 +16,16 @@ export class DjikstraScreen extends Container {
   public mainContainer: Container;
   private clearGoalButton: Button;
   private resetButton: Button;
+  private setGoalButton: Button;
+  private setStartButton: Button;
+
+  private mode: Mode = Mode.NONE;
 
   private grid: Grid;
 
   constructor() {
     super();
-
+    this.mode = Mode.NONE;
     this.mainContainer = new Container();
     this.addChild(this.mainContainer);
 
@@ -24,7 +35,6 @@ export class DjikstraScreen extends Container {
       height: 110,
     });
     this.clearGoalButton.onPress.connect(() => {
-      console.log("Clear Goal button pressed");
       // Logic to clear the goal in the Djikstra algorithm
     });
     this.addChild(this.clearGoalButton);
@@ -40,6 +50,28 @@ export class DjikstraScreen extends Container {
     });
     this.addChild(this.resetButton);
 
+    this.setGoalButton = new Button({
+      text: "Set Goal",
+      width: 175,
+      height: 110,
+    });
+    this.setGoalButton.onPress.connect(() => {
+      this.mode = Mode.SET_GOAL;
+      console.log("Set Goal button pressed");
+    });
+    this.addChild(this.setGoalButton);
+
+    this.setStartButton = new Button({
+      text: "Set Start",
+      width: 175,
+      height: 110,
+    });
+    this.setStartButton.onPress.connect(() => {
+      this.mode = Mode.SET_START;
+      console.log("Set Start button pressed");
+    });
+    this.addChild(this.setStartButton);
+
     this.grid = this.initializeGrid(10, 10, 75, 75);
     this.grid.x = (-this.grid.tileWidth * this.grid.cols) / 2;
     this.grid.y = (-this.grid.tileHeight * this.grid.rows) / 2 - 50;
@@ -51,7 +83,13 @@ export class DjikstraScreen extends Container {
     tileWidth: number,
     tileHeight: number
   ): Grid {
-    const grid = new Grid(rows, cols, tileWidth, tileHeight);
+    const grid = new Grid(
+      rows,
+      cols,
+      tileWidth,
+      tileHeight,
+      this.cellClickHandler.bind(this)
+    );
     this.mainContainer.addChild(grid);
     return grid;
   }
@@ -60,11 +98,30 @@ export class DjikstraScreen extends Container {
     const centreX = width / 2;
     const centreY = height / 2;
 
+    const actions = [
+      this.clearGoalButton,
+      this.resetButton,
+      this.setGoalButton,
+      this.setStartButton,
+    ];
+
     this.mainContainer.x = centreX;
     this.mainContainer.y = centreY;
-    this.clearGoalButton.x = centreX - 200;
-    this.clearGoalButton.y = height - 100;
-    this.resetButton.x = centreX + 200;
-    this.resetButton.y = height - 100;
+
+    const actionsStartX = centreX - (actions.length * 175) / 2 + 87.5;
+    const actionsStartY = height - 100;
+    const actionsGap: number = 10;
+    actions.forEach((action, index) => {
+      action.x = actionsStartX + index * (action.width + actionsGap);
+      action.y = actionsStartY;
+    });
   }
+
+  public cellClickHandler(tile: Tile) {
+    console.log(`Tile clicked at (${tile.xCoord}, ${tile.yCoord})`);
+    tile.onMouseDown(this.mode);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public update(_time: Ticker) {}
 }
